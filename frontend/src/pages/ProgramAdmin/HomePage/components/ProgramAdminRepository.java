7ccf08e4eb5617f6b5cb53a9fa09ccd82d4d4c87
Ciddy Macapinlac <zed.maxcapinlac@gmail.com>
2025-09-05 23:05:06 +0800

    removed unnecessary logs

--- frontend/src/pages/ProgramAdmin/HomePage/components/ProgramAdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.caps.eteeapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.caps.eteeapp.model.ProgramAdmin;
-
-public interface ProgramAdminRepository extends JpaRepository<ProgramAdmin, Long> {
-    // Additional query methods can be added here if needed
-}
