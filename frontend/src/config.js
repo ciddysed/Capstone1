cb9226c549ca81c24aa5ee1539d87b0a74798f36
Jeskunnn <137254198+Jeskunnn@users.noreply.github.com>
2026-01-25 01:33:51 +0800

    small change on config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // Central config for frontend environment values
-export const BACKEND_URL = 'https://eteeap-foth.onrender.com/';
+export const BACKEND_URL = 'https://eteeap-foth.onrender.com';
 export const API_BASE = `${BACKEND_URL}/api`;
